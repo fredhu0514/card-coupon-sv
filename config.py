05734c6bf4b4030ae1be36d09ca126ad09e7a46a
Fred Hu <huyuqi0514@126.com>
2023-08-03 21:20:33 -0400

    development setup

--- config.py
+++ config.py
@@ -13,8 +13,14 @@ class TestingConfig(Config):
     DEBUG = True
     TESTING = True
 
+class DevelopmentConfig(Config):
+    DEBUG = True
+    TESTING = False
+
 # Determine the current environment based on the 'FLASK_ENV' environment variable
 if os.environ.get("FLASK_ENV") == "production":
     app_config = ProductionConfig()
 elif os.environ.get("FLASK_ENV") == "testing":
     app_config = TestingConfig()
+elif os.environ.get("FLASK_ENV") == "development":
+    app_config = DevelopmentConfig()
